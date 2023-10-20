694f5bde28e018a90d99addd52a8dbc653ed87f9
kazvee <kayvee123@gmail.com>
2023-10-19 21:56:00 -0600

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,23 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a Store Name:"
+new_store_name = gets.chomp
+
+puts "Please enter the Annual Revenue:"
+new_store_revenue = gets.chomp                              # <= "Annual revenue can't be blank" & "Annual revenue is not a number" error if left blank
+
+puts "Does the store carry men's apparel? (true/false):"    # <= "Store must carry at least one of men's or women's apparel" error if left blank
+mens_apparel = gets.chomp
+
+puts "Does the store carry women's apparel? (true/false):"  # <= "Store must carry at least one of men's or women's apparel" error if left blank
+womens_apparel = gets.chomp
+
+new_store = Store.create(name: new_store_name, annual_revenue: new_store_revenue, mens_apparel: mens_apparel, womens_apparel: womens_apparel)
+
+unless new_store.save
+  puts "Error: "
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
